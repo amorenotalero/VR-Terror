f703d59eea0893ca39477e094de594fe63216bd1
Carlos Cuevas <ccuevasbalinas@gmail.com>
2022-05-22 19:03:05 +0200

    Datos

--- VR-TerrorGame/Assets/3_SCRIPTS/GameDataManager.cs
+++ VR-TerrorGame/Assets/3_SCRIPTS/GameDataManager.cs
@@ -9,19 +9,40 @@ namespace HFPS.Systems
 
         public InteractiveItem linterna;
         public InteractiveItem llave;
-
-        //public TriggerObjective electricity;
         public JumpscareTrigger lightTrigger;
-
         public DynamicObject electricitySwither;
-
         public FileExporter fileExporter;
 
+        // TIEMPOS 
         float timer = 0.0f;
+        float timerExploracion = 0.0f;
+        float timerReaccion = 0.0f;
+        bool keepRunningTimer = false;
+
         float timerLinterna = 0.0f;
         float timerLlave = 0.0f;
 
-        bool keepRunningTimer= false;
+        
+        float timerSalon = 0.0f;
+        bool keepRunningTimerSalon = false;
+        int flagSalon = 0;
+
+        float timerComedor = 0.0f;
+        bool keepRunningTimerComedor = false;
+        int flagComedor = 0;
+
+        float timerBano = 0.0f;
+        bool keepRunningTimerBano = false;
+        int flagBano = 0;
+
+        float timerCocina = 0.0f;
+        bool keepRunningTimerCocina = false;
+        int flagCocina = 0;
+
+        float timerDormitorio = 0.0f;
+        bool keepRunningTimerDormitorio = false;
+        int flagDormitorio = 0;
+        int flagDormitorio2 = 0;
 
         bool hasLinterna;
         bool hasLlave;
@@ -33,11 +54,78 @@ namespace HFPS.Systems
             hasLlave = false;
         }
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.tag == "MISION" && flagSalon != 1)
+            {
+                keepRunningTimerSalon = true;
+                flagSalon =  1;
+                Debug.Log("BUSCA SALON");
+            }
+            if (other.tag == "SALON" && flagComedor != 1)
+            {
+                keepRunningTimerSalon = false;
+                keepRunningTimerComedor = true;
+                flagComedor = 1;
+                Debug.Log("BUSCA COMEDOR");
+            }
+            if (other.tag == "COMEDOR" && flagBano != 1)
+            {
+                keepRunningTimerComedor = false;
+                keepRunningTimerBano = true;
+                flagBano = 1;
+                Debug.Log("BUSCA BAÑO");
+            }
+            if (other.tag == "BANO" && flagCocina !=1)
+            {
+                keepRunningTimerBano=false;
+                keepRunningTimerCocina=true;
+                flagCocina = 1;
+                Debug.Log("BUSCA COCINA");
+            }
+            if (other.tag == "COCINA" && flagDormitorio !=1)
+            {
+                keepRunningTimerCocina=false;
+                keepRunningTimerDormitorio = true;
+                flagDormitorio=1;
+                Debug.Log("BUSCA DORMITORIO");
+            }
+            if (other.tag == "DORMITORIO" && flagDormitorio2 !=1)
+            {
+                keepRunningTimerDormitorio=false;
+                timerExploracion = timer;
+                flagDormitorio2 = 1;
+                Debug.Log("LUCES");
+            }
+        }
+
         void Update()
         {
             if (keepRunningTimer)
             {
                 timer = timer + Time.deltaTime;
+
+                if(keepRunningTimerSalon)
+                {
+                    timerSalon = timerSalon + Time.deltaTime;
+                }
+                if (keepRunningTimerComedor)
+                {
+                    timerComedor = timerComedor + Time.deltaTime;
+                }
+                if (keepRunningTimerBano)
+                {
+                    timerBano = timerBano + Time.deltaTime;
+                }
+                if (keepRunningTimerCocina)
+                {
+                    timerCocina = timerCocina + Time.deltaTime;
+                }
+                if (keepRunningTimerDormitorio)
+                {
+                    timerDormitorio = timerDormitorio + Time.deltaTime;
+                }
+
                 if (lightTrigger.isPlayed)
                 {
                     if (hasLinterna == false)
@@ -46,7 +134,7 @@ namespace HFPS.Systems
                     }
                     if (hasLlave == false)
                     {
-                        timerLlave = timerLinterna + Time.deltaTime;
+                        timerLlave = timerLlave + Time.deltaTime;
                     }
                     if (linterna.itemTaken == true)
                     {
@@ -59,14 +147,48 @@ namespace HFPS.Systems
                     if (electricitySwither.isUp)
                     {
                         keepRunningTimer = false;
-                        string data = "tiempo_total,tiempo_linterna,tiempo_llave";
-                        string time1 = formatTime(timer);
-                        string time2 = formatTime(timerLinterna);
-                        string time3 = formatTime(timerLlave);
-                        string dataTime = time1 + "," + time2 + "," + time3;
+
                         List<string> list = new List<string>();
-                        list.Add(data);
-                        list.Add(dataTime);
+
+                        string datosHabitaciones = "t_salon,t_comedor,t_aseo,t_cocina,t_dormitorio";
+                        string timeSalon = formatTime(timerSalon);
+                        string timeComedor = formatTime(timerComedor);
+                        string timeBano = formatTime(timerBano);
+                        string timeCocina = formatTime(timerCocina);
+                        string timeDormitorio = formatTime(timerDormitorio);
+                        string timeHabitaciones = timeSalon + "," + timeComedor + "," + timeBano + "," + timeCocina + "," + timeDormitorio;
+
+                        list.Add(datosHabitaciones);
+                        list.Add(timeHabitaciones);
+
+                        string datosExploracion = "tiempo_exploracion";
+                        string timeExploracion = formatTime(timerExploracion);
+
+                        list.Add(datosExploracion);
+                        list.Add(timeExploracion);
+
+                        string datosObjetos = "t_linterna,t_llave";
+                        string timeLinterna = formatTime(timerLinterna);
+                        string timeLlave = formatTime(timerLlave);
+                        string timeObjetos = timeLinterna + "," + timeLlave;
+
+                        list.Add(datosObjetos);
+                        list.Add(timeObjetos);
+
+                        string datosReaccion = "tiempo_reaccion";
+                        timerReaccion = timer - timerExploracion;
+                        string timeReaccion = formatTime(timerReaccion);
+
+                        list.Add(datosReaccion);
+                        list.Add(timeReaccion);
+
+                        string datosTotal = "tiempo_total";
+                        string timeTotal = formatTime(timer);
+
+                        list.Add(datosTotal);
+                        list.Add(timeTotal);
+
+                        Debug.Log("DATOS REGISTRADOS");
                         fileExporter.writeCSV("stats.csv", list);
                     }
                 }
